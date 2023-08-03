6c35761b866618cdd04674cbdc85f51c1063904c
Serhii <brdnk.srh@gmail.com>
2023-08-03 16:41:39 +0300

    expenses list imtems implemented

--- components/ExpensesOutput/ExpensesOutput.jsx
+++ components/ExpensesOutput/ExpensesOutput.jsx
@@ -35,6 +35,36 @@ const DUMMY_EXPENSES = [
         amount: 18.59,
         date: new Date("2022-02-18"),
     },
+    {
+        id: "e7",
+        description: "A pair of shoes",
+        amount: 59.99,
+        date: new Date("2021-12-19"),
+    },
+    {
+        id: "e6",
+        description: "A pair of trousers",
+        amount: 89.29,
+        date: new Date("2022-01-05"),
+    },
+    {
+        id: "e8",
+        description: "Some bananas",
+        amount: 5.99,
+        date: new Date("2021-12-01"),
+    },
+    {
+        id: "e9",
+        description: "A book",
+        amount: 14.99,
+        date: new Date("2022-02-19"),
+    },
+    {
+        id: "e10",
+        description: "Another book",
+        amount: 18.59,
+        date: new Date("2022-02-18"),
+    },
 ];
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
@@ -53,7 +83,9 @@ const { colors } = GlobalStyles;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 24,
+        paddingHorizontal: 24,
+        paddingTop: 24,
+        paddingBottom: 0,
         backgroundColor: colors.primary700,
     },
 });
